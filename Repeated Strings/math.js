9a9a7163262dca287cc2ad487bc7adde3e1c9393
moskowitza <moskowitzaaron@mac.com>
2018-11-13 12:57:29 -0500

    found working solution to repeated string

--- Repeated Strings/math.js
+++ Repeated Strings/math.js
@@ -3,14 +3,24 @@ var n = 10; //the number of characters we'll count
 // Complete the repeatedString function below.
 function repeatedString(s, n) {
   let asInString = 0;
+  let remainderAs=0;
   let sArr = s.split("");
-let remainder=n/s.length;
-console.log(remainder);
-    for (let i = 0; i < sArr.length; i++) {
-      if (longArr.length <= n && sArr[i] === "a") {
-        asInString++;
-      }
+  let remainder = n % sArr.length;
+  let multiplier=Math.floor(n / sArr.length);
+console.log("multiplier: "+multiplier)
+  console.log("remainder: "+remainder);
+  for (let i = 0; i < sArr.length; i++) {
+    if (sArr[i] == "a") {
+      asInString++;
+    }
+  }
+  for (let i = 0; i < remainder; i++) {
+    if (sArr[i] == "a") {
+        remainderAs++;
+    }
+  }
 
+  return asInString*multiplier+remainderAs;
 }
 
 console.log(repeatedString(s, n));
