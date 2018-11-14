99fd7022344d8031c7c364855767bc00d50fe266
moskowitza <moskowitzaaron@mac.com>
2018-11-14 17:26:59 -0500

    oh man

--- Arrays/Rotation Arrays/mySolution.js
+++ Arrays/Rotation Arrays/mySolution.js
@@ -1,12 +1,11 @@
-let testArray=[1,2,3,4,5]
-let integer=2
+let testArray = [1, 2, 3, 4, 5];
+let integer = 2;
 function rotLeft(a, d) {
-
-    for(let i=0;i<d;i++){
-let firstElement=a.shift()
-a.push(firstElement)
-    }
-return a;
+  for (let i = 0; i < d; i++) {
+    let firstElement = a.shift();
+    a.push(firstElement);
+  }
+  return a;
 }
 
-console.log(rotLeft(testArray,integer))
+console.log(rotLeft(testArray, integer));
